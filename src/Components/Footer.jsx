f228b871ab42a505629a2532922fc59128fd394d
Mdmahadi71 <habibalmasud79@gmail.com>
2024-10-22 05:33:17 -0700

    demo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { FaFacebook ,FaTwitter ,FaLinkedin ,FaYoutube  } from "react-icons/fa";
+import { FaFacebook ,FaTwitter ,FaLinkedin ,FaYoutube ,FaPhoneAlt  } from "react-icons/fa";
+import { BsCalendar2DateFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className=' bg-black'>
+  <>
+    <div className=' bg-black py-[40px]'>
         <div className=" container">
             <div className=" flex justify-between items-center py-[30px]">
                 <div className="w-[35%]">
@@ -17,7 +21,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="">
+            <div className=" flex justify-between ">
                 <div className="w-[24%]">
                     <div className="">
                           <h2 className=' font-Dm-sans font-bold text-[25px] text-white  border-b-[2px] border-Agency-liniar my-2'>About Company</h2>
@@ -45,9 +49,48 @@ const Footer = () => {
                       </ul>
                     </div>
                 </div>
+
+                  <div className="w-[24%]">
+                      <div className="">
+                          <h2 className=' font-Dm-sans font-bold text-[25px] text-white  border-b-[2px] border-Agency-liniar my-2'>Recent Post</h2>
+                          <div className=" flex items-center gap-x-2">
+                            <div className=" w-[70px] h-[70px] bg-slate-400 rounded-[5px]"></div>
+                            <div className="">
+                                <p className=' font-Dm-sans font-normal text-[16px] text-white'>Thoughtful man using laptop pondering </p>
+                                <p className=' text-white flex items-center gap-x-2 font-Dm-sans font-normal text-[16px]'><BsCalendar2DateFill className=' text-Agency-blu'/> 09 Dec 2022</p>
+                            </div>
+                          </div>
+                          <div className=" flex items-center gap-x-2 py-3">
+                            <div className=" w-[70px] h-[70px] bg-slate-400 rounded-[5px]"></div>
+                            <div className="">
+                                <p className=' font-Dm-sans font-normal text-[16px] text-white'>Thoughtful man using laptop pondering </p>
+                                <p className=' text-white flex items-center gap-x-2 font-Dm-sans font-normal text-[16px]'><BsCalendar2DateFill className=' text-Agency-blu'/> 09 Dec 2022</p>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+                <div className="w-[24%]">
+                    <div className="">
+                       <h2 className=' font-Dm-sans font-bold text-[25px] text-white  border-b-[2px] border-Agency-liniar my-2'>Contact Us</h2>
+                       <p className=' font-Dm-sans font-normal text-[16px] text-white flex gap-x-4 items-center'><FaPhoneAlt  className=' text-Agency-blu'/>  +(323) 750-1234</p>
+                       <p className=' font-Dm-sans font-normal text-[16px] text-white flex gap-x-4 items-center'><MdEmail className=' text-Agency-blu'/> infoyourortencey@gmail.com</p>
+                       <p className=' font-Dm-sans font-normal text-[16px] text-white flex gap-x-4 items-center'><FaLocationDot className=' text-Agency-blu'/> 374 FA Tower, William S Blvd, Melbourne
+                       2721, IL, USA</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+  <div className=" bg-Agency-blu">
+    <div className=" container">
+        <div className=" flex justify-between items-center py-[30px]">
+            <p className=' font-Dm-sans font-normal text-[16px] text-white'>2022 © All rights reserved by devsnest-llc</p>
+            <p className='font-Dm-sans font-normal text-[16px] text-white'>Privacy Policy       |       Terms & Conditions</p>
+         
+        </div>
+    </div>
+  </div>
+  </>
   )
 }
 
