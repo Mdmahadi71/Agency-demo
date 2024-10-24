d5032f54ff223ab5ee41c5dde485498960b4b11a
Mdmahadi71 <habibalmasud79@gmail.com>
2024-10-24 09:37:56 -0700

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { createBrowserRouter,createRoutesFromElements ,Route ,RouterProvider} from 'react-router-dom'
 import Layout from './Components/Layout'
 import Home from './Pages/Home'
+import About from './Pages/About'
 
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route element={ <Layout/>}>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/About' element={<About/>}></Route>
     </Route>
   ))
 
---
+++ src/Components/AboutMain.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutMain = () => {
+  return (
+    <div>
+      <div className=" container">
+        <div className="">
+            <div className="w-[23%]">
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis quisquam eius, est ab facilis maiores sequi delectus quae quod suscipit!</h3>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMain
